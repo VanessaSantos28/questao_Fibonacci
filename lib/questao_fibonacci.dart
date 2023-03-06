a420ff3045f7f5f8793ce3d80d51e5166b7424f5
Vanessa Santos <vanessa.mss@hotmail.com.br>
2023-03-06 18:46:04 -0300

    start commit

---
+++ lib/questao_fibonacci.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+List<int> fibonacciSequence(int n) {
+  List<int> fib = [0, 1];
+  while (fib.last < n) {
+    fib.add(fib[fib.length - 1] + fib[fib.length - 2]);
+  }
+  return fib;
+}
+
+bool isFibonacciNumber(int x) {
+  List<int> fibNumber = fibonacciSequence(x);
+  return fibNumber.contains(x);
+}
+
+void main() {
+  int number = 28;
+  List<int> finalFibSequence = fibonacciSequence(number);
+  print("A sequência de Fibonacci até $number é: $finalFibSequence");
+  if (isFibonacciNumber(number)) {
+    print("$number faz parte da sequência de fibonacci.");
+  } else {
+    print("$number não faz parte da sequência de Fibonacci.");
+  }
+}
